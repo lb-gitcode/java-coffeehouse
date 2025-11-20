72b055b0def5e2aa377064bfa76f5fc54f54b882
LB <lboyko@student.mitt.ca>
2025-11-19 18:02:37 -0600

    Added all items as objects, cart fully functional

--- assets/script/app.js
+++ assets/script/app.js
@@ -43,11 +43,33 @@ listen('click', loginSubmit, function() {
 const espresso = new Product(0, 'Espresso', 3.50);
 const americano = new Product(1, 'Americano', 4);
 const cappucino = new Product(2, 'Cappucino', 4.75);
+const matcha = new Product(3, 'Matcha Latte', 5.25);
+const icedCoffee = new Product(4, 'Iced Coffee', 4.50);
+const macchiato = new Product(5, 'Macchiato', 5.50);
+const flatWhite = new Product(6, 'Flat White', 4.90);
+const mocha = new Product(7, 'Mocha', 5.35);
 
-let products = [
-  espresso,
-  americano,
-  cappucino
+const lemonLoaf = new Product(8, 'Lemon Loaf', 3.75);
+const croissant = new Product(9, 'Croissant', 3.25);
+const bananaBread = new Product(10, 'Banana Bread', 3.50);
+const cinnamonRoll = new Product(11, 'Cinnamon Roll', 4.25);
+const macaronBox = new Product(12, 'Macaron Box', 9);
+const brownie = new Product(13, 'Brownie', 3.75);
+const macaronBits = new Product(14, 'Macaron Bits', 2.75);
+const cheesecakeSlice = new Product(15, 'Cheesecake Slice', 4.95);
+const oreoShake = new Product(16, 'Oreo Shake', 5.75);
+const chocoShake = new Product(17, 'Chocolate Shake', 7.75);
+const brewIcedCoffee = new Product(18, 'Brewed Iced Coffee', 9.75);
+const chocoIcedCoffee = new Product(19, 'Choco Iced Coffee', 3.75);
+const coffeeCombo = new Product(20, 'Coffee Combo', 7.50);
+const shakeCombo = new Product(21, 'Shake Combo', 8.50);
+const donuts = new Product(22, 'Donuts', 9.75);
+
+let products = [ 
+  espresso, americano, cappucino, matcha, icedCoffee, macchiato, flatWhite, mocha,
+  lemonLoaf, croissant, bananaBread, cinnamonRoll, macaronBox, brownie, macaronBits, cheesecakeSlice,
+  oreoShake, chocoShake, brewIcedCoffee, chocoIcedCoffee,
+  coffeeCombo, shakeCombo, donuts
 ];
 
 console.log(products);
